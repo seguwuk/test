3c8dc68dbbcf356356e4c896b6266bf514be631c
seguwuk <seguwuk@gmail.com>
2023-07-27 07:50:36 -0300

    page2 sem funcao

--- src/app/pages/door/porta2/porta2.component.ts
+++ src/app/pages/door/porta2/porta2.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../../services/game.service';
 
 @Component({
   selector: 'app-porta2',
   templateUrl: './porta2.component.html',
   styleUrls: ['./porta2.component.css']
 })
-export class Porta2Component {
+export class Porta2Component implements OnInit {
+  constructor(public gameService: GameService) {}
 
+  ngOnInit(): void {}
+
+  unlockLock(lockNumber: number): void {
+    this.gameService.unlockSmallLock('porta2', lockNumber);
+  }
+
+  isLockUnlocked(lockNumber: number): boolean {
+    return this.gameService.isSmallLockUnlocked('porta2', lockNumber);
+  }
+
+  unlockBigLock(): void {
+    this.gameService.unlockBigLock('porta2');
+  }
+
+  isBigLockUnlocked(): boolean {
+    return this.gameService.isBigLockUnlocked('porta2');
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -4,29 +4,85 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GameService {
+  private unlockSequence: string[] = ['porta1', 'porta2', 'porta3', 'gato'];
   private unlockedPages: string[] = [];
+  private unlockedSmallLocks: { [page: string]: number[] } = {};
+  private unlockedBigLocks: { [page: string]: boolean } = {};
+
+  constructor() {}
+
+  unlockPage(page: string): void {
+    if (this.isPageUnlocked(page)) return;
+    const expectedPage = this.unlockSequence[this.unlockedPages.length];
+    if (page === expectedPage) {
+      this.unlockedPages.push(page);
+    } else {
+      this.resetProgress();
+    }
+  }
+
+  unlockSmallLock(page: string, lockNumber: number): void {
+    // Verificar se a página já foi desbloqueada
+    if (!this.unlockedSmallLocks[page]) {
+      this.unlockedSmallLocks[page] = [];
+    }
+
+    if (this.unlockedSmallLocks[page].includes(lockNumber)) {
+      // Se o cadeado já estiver desbloqueado, não fazer nada
+      return;
+    }
+
+    // Verificar se o cadeado é o próximo a ser desbloqueado
+    const expectedLockNumber = this.unlockedSmallLocks[page].length + 1;
+    if (lockNumber === expectedLockNumber) {
+      this.unlockedSmallLocks[page].push(lockNumber);
+
+      // Verificar se todos os cadeados pequenos da página foram desbloqueados
+      const totalSmallLocks = 3; // Total de cadeados pequenos por página
+      if (this.unlockedSmallLocks[page].length === totalSmallLocks) {
+        this.unlockBigLock(page);
+      }
+    } else {
+      // Se o cadeado não é o próximo a ser desbloqueado, reiniciar o progresso
+      this.resetProgress();
+    }
+  }
+
+  unlockBigLock(page: string): void {
+    if (!this.unlockedPages.includes(page)) {
+      // Se a página não foi desbloqueada, reiniciar o progresso
+      this.resetProgress();
+      return;
+    }
+
+    if (this.unlockedBigLocks[page]) {
+      // Se o cadeado grande já estiver desbloqueado, não fazer nada
+      return;
+    }
+
+    this.unlockedBigLocks[page] = true;
+
+    if (Object.keys(this.unlockedBigLocks).length === this.unlockSequence.length) {
+      // Se todos os cadeados grandes foram desbloqueados, desbloquear a última página (gato)
+      this.unlockPage('gato');
+    }
+  }
 
   isPageUnlocked(page: string): boolean {
     return this.unlockedPages.includes(page);
   }
 
-  unlockPage(page: string): void {
-    if (!this.isPageUnlocked(page)) {
-      this.unlockedPages.push(page);
-    }
+  isSmallLockUnlocked(page: string, lockNumber: number): boolean {
+    return this.unlockedSmallLocks[page]?.includes(lockNumber);
   }
 
-  unlockSmallLock(page: string, lockNumber: number): void {
-    const smallLock = `${page}_cadeado${lockNumber}`;
-    if (!this.isPageUnlocked(smallLock)) {
-      this.unlockedPages.push(smallLock);
-    }
+  isBigLockUnlocked(page: string): boolean {
+    return this.unlockedBigLocks[page] || false;
   }
 
-  unlockBigLock(page: string): void {
-    const bigLock = `${page}_grande`;
-    if (!this.isPageUnlocked(bigLock)) {
-      this.unlockedPages.push(bigLock);
-    }
+  private resetProgress(): void {
+    this.unlockedPages = [];
+    this.unlockedSmallLocks = {};
+    this.unlockedBigLocks = {};
   }
 }
