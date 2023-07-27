3515d1e31f665567763820d3d176302c0258dd39
seguwuk <seguwuk@gmail.com>
2023-07-27 06:36:12 -0300

    cadeados

--- src/app/pages/door/porta1/porta1.component.ts
+++ src/app/pages/door/porta1/porta1.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { GameService } from '../../../services/game.service';
+import { Component } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-porta1',
   templateUrl: './porta1.component.html',
   styleUrls: ['./porta1.component.css']
 })
-export class Porta1Component implements OnInit {
+export class Porta1Component {
   constructor(private gameService: GameService) {}
 
-  ngOnInit(): void {}
+  openSmallLock(lockNumber: number) {
+    this.gameService.unlockSmallLock('porta1', lockNumber);
+  }
 
   isSmallLockOpen(lockNumber: number): boolean {
-    return this.gameService.isPageUnlocked(`porta1_lock${lockNumber}`);
+    return this.gameService.isSmallLockUnlocked('porta1', lockNumber);
   }
 
-  openSmallLock(lockNumber: number): void {
-    if (!this.isSmallLockOpen(lockNumber)) {
-      this.gameService.unlockPage(`porta1_lock${lockNumber}`);
-    }
-  }
-
-  get isBigLockOpen(): boolean {
-    return this.gameService.isPageUnlocked('porta1');
+  isBigLockOpen(): boolean {
+    return this.gameService.isBigLockUnlocked('porta1');
   }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GameService {
   private unlockSequence: string[] = ['porta1', 'porta2', 'porta3', 'gato'];
   private unlockedPages: string[] = [];
-
-  constructor() {}
+  private smallLocks: boolean[] = [false, false, false];
+  private bigLockOpen = false;
 
   isPageUnlocked(page: string): boolean {
-    return this.unlockedPages.includes(page);
+    const pageIndex = this.unlockedPages.indexOf(page);
+    return pageIndex !== -1;
   }
 
   unlockPage(page: string): void {
-    const pageIndex = this.unlockSequence.indexOf(page);
-    if (pageIndex !== -1) {
-      const previousPages = this.unlockSequence.slice(0, pageIndex);
-      if (previousPages.every(page => this.unlockedPages.includes(page))) {
-        this.unlockedPages.push(page);
-      }
+    this.unlockedPages.push(page);
+  }
+
+  isSmallLockOpen(lockNumber: number): boolean {
+    return this.smallLocks[lockNumber - 1];
+  }
+
+  openSmallLock(lockNumber: number): void {
+    if (this.isSmallLockOpen(lockNumber - 1)) {
+      this.smallLocks[lockNumber - 1] = true;
+    } else {
+      this.smallLocks.fill(false, lockNumber - 1);
     }
   }
 
-  lockPage(page: string): void {
-    this.unlockedPages = this.unlockedPages.filter(p => p !== page);
+  get isBigLockOpen(): boolean {
+    return this.bigLockOpen;
+  }
+
+  openBigLock(): void {
+    if (this.smallLocks.every(lock => lock)) {
+      this.bigLockOpen = true;
+      this.unlockPage('porta1');
+    }
   }
 }
