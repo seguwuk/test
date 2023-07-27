a10a67765f50b0be587581c0a461580e1770dd0b
seguwuk <seguwuk@gmail.com>
2023-07-27 06:01:45 -0300

    fontawesome

--- src/app/pages/door/porta1/porta1.component.ts
+++ src/app/pages/door/porta1/porta1.component.ts
@@ -1,46 +1,27 @@
-import { Component } from '@angular/core';
-import { GameService } from 'src/app/services/game.service';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../../services/game.service';
 
 @Component({
   selector: 'app-porta1',
   templateUrl: './porta1.component.html',
   styleUrls: ['./porta1.component.css']
 })
-export class Porta1Component {
-  private cadeadosPequenosAbertos = [false, false, false];
-  private cadeadoGrandeAberto = false;
-
+export class Porta1Component implements OnInit {
   constructor(private gameService: GameService) {}
 
-  isCadeadoPequenoOpen(index: number): boolean {
-    return this.cadeadosPequenosAbertos[index];
+  ngOnInit(): void {}
+
+  isSmallLockOpen(lockNumber: number): boolean {
+    return this.gameService.isPageUnlocked(`porta1_lock${lockNumber}`);
   }
 
-  isCadeadoGrandeOpen(): boolean {
-    return this.cadeadoGrandeAberto;
-  }
-
-  openCadeadoPequeno(index: number): void {
-    this.cadeadosPequenosAbertos[index] = true;
-    this.checkIfPorta1IsUnlocked();
-  }
-
-  openCadeadoGrande(): void {
-    if (this.areAllCadeadosPequenosOpen()) {
-      this.cadeadoGrandeAberto = true;
-      this.checkIfPorta1IsUnlocked();
+  openSmallLock(lockNumber: number): void {
+    if (!this.isSmallLockOpen(lockNumber)) {
+      this.gameService.unlockPage(`porta1_lock${lockNumber}`);
     }
   }
 
-  private areAllCadeadosPequenosOpen(): boolean {
-    return this.cadeadosPequenosAbertos.every(open => open);
-  }
-
-  private checkIfPorta1IsUnlocked(): void {
-    if (this.isCadeadoGrandeOpen()) {
-      this.gameService.unlockPage('porta1');
-    } else {
-      this.gameService.lockPage('porta1');
-    }
+  get isBigLockOpen(): boolean {
+    return this.gameService.isPageUnlocked('porta1');
   }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class GameService {
   private unlockSequence: string[] = ['porta1', 'porta2', 'porta3', 'gato'];
-
   private unlockedPages: string[] = [];
 
   constructor() {}
@@ -14,18 +13,17 @@ export class GameService {
     return this.unlockedPages.includes(page);
   }
 
-  unlockPage(page: string): boolean {
-    if (!this.unlockedPages.includes(page)) {
-      if (this.isNextPage(page)) {
+  unlockPage(page: string): void {
+    const pageIndex = this.unlockSequence.indexOf(page);
+    if (pageIndex !== -1) {
+      const previousPages = this.unlockSequence.slice(0, pageIndex);
+      if (previousPages.every(page => this.unlockedPages.includes(page))) {
         this.unlockedPages.push(page);
-        return true;
       }
     }
-    return false;
   }
 
-  private isNextPage(page: string): boolean {
-    const currentPageIndex = this.unlockedPages.length;
-    return this.unlockSequence[currentPageIndex] === page;
+  lockPage(page: string): void {
+    this.unlockedPages = this.unlockedPages.filter(p => p !== page);
   }
 }
